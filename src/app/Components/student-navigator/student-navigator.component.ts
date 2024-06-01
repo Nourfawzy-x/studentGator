2129feac036602af93cef4229167f36139ac12d4
nour fawzy <nourfawzy292001@gmail.com>
2024-06-01 04:13:26 +0300

    finish navbar and header compontent

---
+++ src/app/Components/student-navigator/student-navigator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-navigator',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-navigator.component.html',
+  styles: ``
+})
+export class StudentNavigatorComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProgramsComponent } from './Components/programs/programs.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { HomeComponent } from './Components/home/home.component';
+import { StudentNavigatorComponent } from './Components/student-navigator/student-navigator.component';
+import { BlogComponent } from './Components/blog/blog.component';
+import { ErrorComponent } from './Components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProgramsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutUs', component: AboutUsComponent },
+  { path: 'studentNavigator', component: StudentNavigatorComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: '**', component: ErrorComponent },
+];
