4f384939fcbbdb84a56fdf912a68c5702c843e7b
nour fawzy <nourfawzy292001@gmail.com>
2024-06-03 10:48:57 +0300

    .

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WhiteLogoComponent } from '../white-logo/white-logo.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [WhiteLogoComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
---
+++ src/app/Components/white-logo/white-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-white-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './white-logo.component.html',
+  styleUrl: './white-logo.component.css'
+})
+export class WhiteLogoComponent {
+
+}
